6c611066427fed7fa82cd37087f67e137320f335
GustavoSoaresbcge <168493242+GustavoSoaresbcge@users.noreply.github.com>
2025-04-23 21:18:13 -0300

    Integração Banco de dados
    Banco de dados integrado, adição de nome do jogador, interfaces rápidas com Pygame.

--- codigo.py
+++ codigo.py
@@ -1,5 +1,16 @@
 import pygame
 from random import randint
+# Criação do banco de dados com as tabelas
+import sqlite3
+
+conexao = sqlite3.connect('ranking.db')
+cursor = conexao.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS ranking(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nickname TEXT NOT NULL UNIQUE,
+pontos INTEGER NOT NULL)
+''')
 
 # Inicializa o Pygame
 pygame.init()
@@ -30,6 +41,90 @@ speed = 15
 
 
 # Funções
+def tela_inicial(tela, largura, altura, fonte, cursor):
+    nickname = ""
+    digitando = True
+
+    while digitando:
+        tela.fill(preto)
+
+        # Texto fixo
+        mensagem("Digite seu nome:", branco, largura // 2 - 200, altura // 2 - 70)
+        mensagem_intermitente("Confirme com Enter", largura // 2 + 50, altura // 2 + 40)
+        mensagem("Máx. 10 caracteres!", branco, largura // 2 - 200, altura // 2 + 40, True)
+
+        # Retângulo de entrada
+        pygame.draw.rect(tela, branco, (largura // 2 - 200, altura // 2 - 20, 400, 50), 2)
+
+        # Mostrar texto digitado
+        mensagem(nickname, branco, largura // 2 - 190, altura // 2 - 10)
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    nickname = nickname.strip()
+                    if nickname == "":
+                        # Buscar apelidos Anônimo já existentes
+                        cursor.execute("SELECT nickname FROM ranking WHERE nickname LIKE 'Anônimo___'")
+                        anonimos = cursor.fetchall()
+
+                        numeros = []
+                        for (apelido,) in anonimos:
+                            try:
+                                numero = int(apelido.replace("Anônimo", ""))
+                                numeros.append(numero)
+                            except ValueError:
+                                continue
+
+                        proximo = 1
+                        while proximo in numeros:
+                            proximo += 1
+
+                        nickname = f"Anônimo{proximo:03}"
+
+                    elif len(nickname) > 10:
+                        nickname = ""
+                        continue
+
+                    digitando = False
+
+                elif event.key == pygame.K_BACKSPACE:
+                    nickname = nickname[:-1]
+
+                elif len(nickname) < 20:
+                    nickname += event.unicode
+
+    return nickname
+
+
+def adicionar(name, point):
+    cursor.execute("""
+    INSERT INTO ranking (nickname, pontos)
+    VALUES (?, ?)""", (name, point))
+    conexao.commit()
+
+    # O rowid age para desempacotar as tuplas recebidas no fetchall
+    cursor.execute("SELECT rowid, pontos FROM ranking ORDER BY pontos DESC")
+    dados_ordenados = cursor.fetchall()
+    if len(dados_ordenados) > 10: # Pega os IDs que estão além do top 10
+        excedentes = dados_ordenados[10:]
+        for (rowid, _) in excedentes: # _ age para suprir o pontos que foi passado com o rowid
+            cursor.execute("DELETE FROM ranking WHERE rowid = ?", (rowid,))
+        conexao.commit()
+    # pprint.pprint(dados_ordenados)
+
+
+def deletar_tudo():
+    cursor.execute("DELETE FROM ranking")
+    conexao.commit()
+
+
 def mensagem(msg, cor, x, y, pequena=False):
     if pequena:
         texto = fonte_pequena.render(msg, True, cor)
@@ -60,16 +155,18 @@ def cor_apple():
     return type_apple
 
 
-def mensagem_intermitente(texto):
+def mensagem_intermitente(texto, x=largura//2 - 80, y=altura - 40):
     tempo = pygame.time.get_ticks()
     if (tempo // 500) % 2 == 0:
-        mensagem(texto, branco, largura//2 - 80, altura - 40, pequena=True)
+        mensagem(texto, branco, x, y, pequena=True)
 
 
 # Jogo principal
-def jogo():
+def main():
     recorde = 0
     jogando = True
+    # Iniciar com a tela inicial
+    nickname = tela_inicial(tela, largura, altura, fonte, cursor)
 
     while jogando:
         game_over = False
@@ -182,6 +279,22 @@ def jogo():
         # Atualiza recorde
         if pontuacao > recorde:
             recorde = pontuacao
+            cursor.execute("SELECT pontos FROM ranking WHERE nickname=?", (nickname,))
+            existe = cursor.fetchone()
+            # Se não existir vai retornar Null e adicionar normal
+            if existe:
+                # Se já existe, e o novo ponto for maior, atualiza
+                if pontuacao > existe[0]:
+                    cursor.execute("UPDATE ranking SET pontos=?"
+                                   " WHERE nickname=?", (pontuacao, nickname))
+                    conexao.commit()
+            else:
+                # Adiciona pontuação no banco de dados se conseguir entrar no ranking ou se tiver menos de 10 itens
+                cursor.execute("SELECT nickname, pontos FROM ranking ORDER BY pontos DESC")
+                dados_ordenados = cursor.fetchall()
+                if len(dados_ordenados) < 10 or (len(dados_ordenados) > 0 and pontuacao > dados_ordenados[-1][1]):
+                    adicionar(nickname, pontuacao)
+                    conexao.commit()
 
         # Fim de jogo (aguardar reinício)
         aguardar_reinicio = True
@@ -189,6 +302,7 @@ def jogo():
             tela.fill(preto)
             mensagem(f'Pontuação: {pontuacao}', branco, largura // 2 - 150, altura // 2 - 60)
             mensagem(f'Recorde pessoal: {recorde}', branco, largura // 2 - 150, altura // 2 - 20)
+            mensagem('Aperte r para visualizar o Ranking', branco, largura // 2 - 150, altura // 2 + 20, pequena=True)
             mensagem_intermitente('Ande para começar')
             pygame.display.update()
 
@@ -199,11 +313,40 @@ def jogo():
                 elif event.type == pygame.KEYDOWN:
                     if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
                         aguardar_reinicio = False
+
+                    # Mostrar Ranking do banco de dados apertando r pra visualizar e sair
+                    elif event.key == pygame.K_r:
+                        show_ranking = True
+                        while show_ranking:
+                            for evento in pygame.event.get():
+                                if evento.type == pygame.QUIT:
+                                    jogando = False
+                                    aguardar_reinicio = False
+                                    show_ranking = False
+                                elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_r:
+                                    show_ranking = False
+
+                            cursor.execute("SELECT nickname, pontos FROM ranking ORDER BY pontos DESC")
+                            em_ordem = cursor.fetchall()
+                            tela.fill(preto)
+                            # Exibir cada linha do ranking
+                            for i in range(len(em_ordem)):
+                                texto = f"{i + 1}º {em_ordem[i][0]}{em_ordem[i][1]} pts"
+                                pontinhos = '.' * (30 - len(texto.strip()))
+                                new_texto = f"{i + 1}º {em_ordem[i][0]}{pontinhos}{em_ordem[i][1]} pts"
+                                # Cada linha desce 40 pixels pra não sobrepor
+                                mensagem(new_texto, branco, largura // 2 - 200, 80 + i * 40)
+
+                            mensagem_intermitente('Pressione r para voltar')
+                            pygame.display.update()
+
+    cursor.close()
+    conexao.close()
     pygame.quit()
     quit()
 
 
 # Inicia o jogo
-jogo()
+main()
 
 
